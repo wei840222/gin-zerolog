47e5916dfe212a501e68320433cad82867534e75
wei <wei840222@gmail.com>
2021-03-03 11:05:20 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/wei840222/gin-zerolog
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/rs/zerolog v1.20.0
+)
---
+++ logger.go
@@ -0,0 +1,74 @@
+package ginlogrus
+
+import (
+	"math"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
+)
+
+// Logger is the logrus logger handler
+func Logger(logger zerolog.Logger, notLogged ...string) gin.HandlerFunc {
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "unknow"
+	}
+
+	var skip map[string]struct{}
+
+	if length := len(notLogged); length > 0 {
+		skip = make(map[string]struct{}, length)
+
+		for _, p := range notLogged {
+			skip[p] = struct{}{}
+		}
+	}
+
+	return func(c *gin.Context) {
+		// other handler can change c.Path so:
+		path := c.Request.URL.Path
+		start := time.Now()
+		c.Next()
+		stop := time.Since(start)
+		latency := int(math.Ceil(float64(stop.Nanoseconds()) / 1000000.0))
+		statusCode := c.Writer.Status()
+		clientIP := c.ClientIP()
+		clientUserAgent := c.Request.UserAgent()
+		referer := c.Request.Referer()
+		dataLength := c.Writer.Size()
+		if dataLength < 0 {
+			dataLength = 0
+		}
+
+		if _, ok := skip[path]; ok {
+			return
+		}
+
+		entry := map[string]interface{}{
+			"hostname":   hostname,
+			"statusCode": statusCode,
+			"latency":    latency, // time to process
+			"clientIP":   clientIP,
+			"method":     c.Request.Method,
+			"path":       path,
+			"referer":    referer,
+			"dataLength": dataLength,
+			"userAgent":  clientUserAgent,
+		}
+
+		if len(c.Errors) > 0 {
+			logger.Error().Msg(c.Errors.ByType(gin.ErrorTypePrivate).String())
+		} else {
+			if statusCode >= http.StatusInternalServerError {
+				logger.Error().Fields(entry).Msg("[GIN] access")
+			} else if statusCode >= http.StatusBadRequest {
+				logger.Warn().Fields(entry).Msg("[GIN] access")
+			} else {
+				logger.Info().Fields(entry).Msg("[GIN] access")
+			}
+		}
+	}
+}
